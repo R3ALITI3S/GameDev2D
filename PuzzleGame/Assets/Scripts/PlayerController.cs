d9505bc944f00fcbf36a78818f12b7bd4fc1fbc4
Malthezz <malthh21@student.aau.dk>
2026-05-27 17:06:15 +0200

    Grapple
    Added grapple mechanic to lvl 2 scene

---
+++ PuzzleGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    [Header("Jump")]
+    public float groundCheckRadius = 0.1f;
+    public LayerMask groundLayer;
+    public float jumpForce;
+
+    [Header("Refs")]
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    //public Animator anim;
+
+    private Vector2 moveInput;
+    private bool isGrounded;
+
+    public static PlayerController Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        // INPUT
+        moveInput = Vector2.zero;
+
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+            moveInput.x = -1;
+
+        if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+            moveInput.x = 1;
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            TryJump();
+
+
+        // Flip the sprite
+        if (moveInput.x != 0)
+            transform.localScale = new Vector3(Mathf.Sign(moveInput.x), 1, 1);
+
+
+        //anim.SetFloat("Speed", Mathf.Abs(rb.linearVelocity.x));
+
+    }
+
+    void FixedUpdate()
+    {
+        if (groundCheck == null) return;
+
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+
+        rb.linearVelocity = new Vector2(
+            moveInput.x * speed,
+            rb.linearVelocity.y
+        );
+    }
+
+    void TryJump()
+    {
+        if (!isGrounded) return;
+
+        //anim.SetTrigger("Jump");
+
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+}
